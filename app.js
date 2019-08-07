f11bfb6cd76a2c1e40e2cdb1a15a7514b0411b8f
Yang Hu <whoyoung@gatech.edu>
2019-08-07 19:11:03 -0400

    err handling: no internet & no match found

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/e2c29712b25f92cd34964b4b2efb3b86/37.8267,-122.4233'
+// const url = 'https://api.darksky.net/forecast/e2c29712b25f92cd34964b4b2efb3b86/37.8267,-122.4233'
 
-request({ url: url, json: true, }, (error, response) => {
-    console.log(response.body.currently.temperature)
-    console.log(response.body.currently.precipProbability)
+// request({ url: url, json: true, }, (error, response) => {
+//     console.log(response.body.currently.temperature)
+//     console.log(response.body.currently.precipProbability)
 
-})
+// })
 
 
-const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=1&access_token=pk.eyJ1IjoibGlua2luZzIwNDYiLCJhIjoiY2p6MXRrcG9jMDEzaDNsbWk4eTl1cXhmMSJ9.5kDYDETiyqiVTU5Iwj4WBw'
+const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/taiwan%20taipei.json?limit=1&access_token=pk.eyJ1IjoibGlua2luZzIwNDYiLCJhIjoiY2p6MXRrcG9jMDEzaDNsbWk4eTl1cXhmMSJ9.5kDYDETiyqiVTU5Iwj4WBw'
 
 request({ url: url2, json: true, }, (error, response) => {
-    console.log(response.body.features[0].center);
+    if (error) {
+        console.log('Cannot access service...')
+    } else if (response.body.features.length === 0) {
+        console.log('Cannot find matching result...')
+    } else {
+        console.log(response.body.features[0].center);
+    }
 })
