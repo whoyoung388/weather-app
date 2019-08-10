96585d6a11e9385e42767df5e643f58441d73086
Yang Hu <whoyoung@gatech.edu>
2019-08-10 14:38:02 -0400

    require an input arg

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const search = process.argv[2]
-
-geocode(search, (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!search) {
+    console.log('Please provide an address.')
+} else {
+    geocode(search, (error, data) => {
         if (error) {
             return console.log(error)
         }
-        console.log(data)
-        console.log(forecastData)
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(data)
+            console.log(forecastData)
+        })
     })
-})
+}
+
