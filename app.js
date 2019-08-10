373ba4d44e312ee1910b0b9b6298494bbcaeb37e
Yang Hu <whoyoung@gatech.edu>
2019-08-10 12:03:58 -0400

    forecast: return temperature & perci% at given lat&long

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = 'https://api.darksky.net/forecast/e2c29712b25f92cd34964b4b2efb3b86/37.8267,-122.4233'
-
-// request({ url: url, json: true, }, (error, response) => {
-//     console.log(response.body.currently.temperature)
-//     console.log(response.body.currently.precipProbability)
-
-// })
-
-geocode('boston', (error, data) => {
+forecast(123, 44, (error, data) => {
     console.log(error)
     console.log(data)
 })
---
+++ utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `https://api.darksky.net/forecast/e2c29712b25f92cd34964b4b2efb3b86/${longitude},${latitude}`
+    request({ url: url, json: true, }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service.')
+        } else if (!response.body.currently) {
+            callback('Bad query input.')
+        } else {
+            const data = {
+                temperature: response.body.currently.temperature,
+                precipitation: response.body.currently.precipProbability,
+                }
+            callback(error, data)
+        }
+    })
+}
+
+module.exports = forecast
