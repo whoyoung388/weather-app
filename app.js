2a6f5a2f0778df27bbd80505e6d75d5045f32e2d
Yang Hu <whoyoung@gatech.edu>
2019-08-10 14:34:17 -0400

    take in arg for query

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-forecast(123, 44, (error, data) => {
-    console.log(error)
-    console.log(data)
+const search = process.argv[2]
+
+geocode(search, (error, data) => {
+    if (error) {
+        return console.log(error)
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(data)
+        console.log(forecastData)
+    })
 })
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,12 +1,12 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/e2c29712b25f92cd34964b4b2efb3b86/${longitude},${latitude}`
+    const url = `https://api.darksky.net/forecast/e2c29712b25f92cd34964b4b2efb3b86/${latitude},${longitude}`
     request({ url: url, json: true, }, (error, response) => {
         if (error) {
             callback('Unable to connect to weather service.')
         } else if (!response.body.currently) {
-            callback('Bad query input.')
+            callback('Bad query input.', response.body)
         } else {
             const data = {
                 temperature: response.body.currently.temperature,
