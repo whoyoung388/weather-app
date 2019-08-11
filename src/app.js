f533e1d2d16ce7aa0cce1caa949319c26507ac12
Yang Hu <whoyoung@gatech.edu>
2019-08-11 15:37:15 -0400

    replace static htmls with handlebars

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
+app.set('view engine', 'hbs')
+// app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        author: 'Yang Hu',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About this page...',
+        author: 'Yang Hu',
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        description: 'Find some helpful info here.',
+        author: 'Yang Hu',
+    })
+})
+
 app.get('/help', (req, res) => {
     res.send('Help page')
 })
