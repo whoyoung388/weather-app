c9f824a836c14c730e73e5666b2a0540f4e98149
Yang Hu <whoyoung@gatech.edu>
2019-08-11 20:56:35 -0400

    bug fix: when no network connection, cannot destructure property

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,13 +3,13 @@ const request = require('request')
 const geocode = (address, callback) => {
     const escapeAddress = encodeURIComponent(address);
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${escapeAddress}.json?limit=1&access_token=pk.eyJ1IjoibGlua2luZzIwNDYiLCJhIjoiY2p6MXRrcG9jMDEzaDNsbWk4eTl1cXhmMSJ9.5kDYDETiyqiVTU5Iwj4WBw`
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, response) => {
         if (error) {
             callback('Unable to connect to location service', undefined)
-        } else if (body.features.length === 0) {
+        } else if (response.body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            const info = body.features[0]
+            const info = response.body.features[0]
             callback(undefined, {
                 latitude: info.center[1],
                 longitude: info.center[0], 
@@ -18,5 +18,10 @@ const geocode = (address, callback) => {
         }
     })
 }
+
+geocode('Atlanta', (error, data) => {
+    console.log(error)
+    console.log(data)
+})
 
 module.exports = geocode
