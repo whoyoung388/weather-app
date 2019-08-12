b2d301046dd095f9f207287a5e4ef9ea549f91c2
Yang Hu <whoyoung@gatech.edu>
2019-08-12 10:14:40 -0400

    use default value for undefined object destructure bug

--- src/app.js
+++ src/app.js
@@ -1,95 +1,92 @@
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
 
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
+const app = express();
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and view location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather App',
-        author: 'Yang Hu',
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    author: "Yang Hu"
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About this page...',
-        author: 'Yang Hu',
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About this page...",
+    author: "Yang Hu"
+  });
+});
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
+app.get("/api/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!"
+    });
+  }
+
+  const address = req.query.address;
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error
+      });
     }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({
+          error
+        });
+      }
+      return res.send({
+        forecast: forecastData,
+        location: location,
+        address: req.query.address
+      });
+    });
+  });
+});
 
-    const address = req.query.address
-    geocode(address, (error, data) => {
-        if (error) {
-            return res.send({
-                error,
-            })
-        }
-        const { latitude, longitude, location } = data
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({
-                    error,
-                })
-            }
-            return res.send({
-                forecast: forecastData,
-                location: location,
-                address: req.query.address,
-            })
-    })
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    description: "Find some helpful info here.",
+    author: "Yang Hu"
+  });
+});
 
-    
-    })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    author: "Yang Hu",
+    errorMessage: "Helper article not found."
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help Page',
-        description: 'Find some helpful info here.',
-        author: 'Yang Hu',
-    })
-})
-
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        author: 'Yang Hu',
-        errorMessage: 'Helper article not found.'
-    })
-})
-
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        author: 'Yang Hu',
-        errorMessage: 'Page not found.',
-    })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    author: "Yang Hu",
+    errorMessage: "Page not found."
+  });
+});
 
 app.listen(4000, () => {
-    console.log('Server is up on port 4000.')
-})
+  console.log("Server is up on port 4000.");
+});
