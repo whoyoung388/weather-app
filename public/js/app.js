9977194d4cc397d35e9f74419eed66d8cceec66f
Yang Hu <whoyoung@gatech.edu>
2019-08-12 12:48:24 -0400

    client side js for fetching forecast from api

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 console.log('test if client side js is loaded...')
+
+fetch('http://localhost:4000/api/weather?address=!').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log({error: data.error})
+        }
+        return console.log(data)
+    })
+})
